da6ef71071b9a5583cdf8d1e98d1d069af081531
Samantha Morco <samanthamorco@gmail.com>
2017-05-09 20:55:31 -0400

    solutions to query parameters exercises

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,41 @@
 class PagesController < ApplicationController
   def query_params_method
-    puts "$$$$$$$$$$"
-    puts params["foobar"]
     @message = params["foobar"]
     @message2 = params[:purple_hippo]
+    if params[:name]
+      @message = "bha"
+    end
     render "query_params.html.erb"
+  end
+
+  def name_game_method
+    if params[:name]
+      @name = params[:name].upcase
+      if @name.starts_with?("A")
+        @message = "Your names starts with A. Cool!"
+      end
+    else
+      @name = "You didn't put in a name!!! Do that!!!"
+    end
+    render "name_game_view.html.erb"
+  end
+
+  def guess_number_method
+    number = 42
+    if params[:number]
+      @guessed_number = params[:number].to_i
+    else
+      @guessed_number = params[:number]
+    end
+    if @guessed_number.nil?
+      @message = "Put in a number dummy."
+    elsif @guessed_number > number
+      @message = "Too high! Try again."
+    elsif @guessed_number == number
+      @message = "Congrats! You guessed it."
+    elsif @guessed_number < number
+      @message = "Too low! Try again."
+    end
+    render "guess_number.html.erb"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/query_params_url", to: "pages#query_params_method"
+  get "/name_game", to: 'pages#name_game_method'
+  get "/guess_a_number", to: 'pages#guess_number_method'
 end
